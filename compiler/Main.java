da034414760b016588c7d6907ff32d6edf43121c
Eric <eric@system76-pc.localdomain>
2021-09-20 11:19:25 -0500

    add copyright and license

--- compiler/Main.java
+++ compiler/Main.java
@@ -1,3 +1,20 @@
+/*
+  The statementLanguage programming language
+  Copyright 2016 Eric J. Deiman
+
+  This file is part of the statementLanguage programming language.
+  The statementLanguage programming language is free software: you can redistribute it
+  and/ormodify it under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+  
+  The statementLanguage programming language is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with the
+  statementLanguage programming language. If not, see <https://www.gnu.org/licenses/>
+*/
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
